86aab3da2349048af5da2de308d1a1b34add9bdf
Hussain <157299514+HussainFlutter@users.noreply.github.com>
2024-06-07 20:10:02 +0500

    Initial commit

---
+++ lib/geolocator_example.dart
@@ -0,0 +1,19 @@
+// Geo locator is a package using which you can get the address of anywhere with coordinates(Lat lng).
+// And vise versa you can get the coordinates (Lat lng) of anywhere if you give it the address.
+//See example below
+
+import 'package:flutter/material.dart';
+
+class GeoLocatorExample extends StatefulWidget {
+  const GeoLocatorExample({super.key});
+
+  @override
+  State<GeoLocatorExample> createState() => _State();
+}
+
+class _State extends State<GeoLocatorExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/markers_example.dart
@@ -0,0 +1,74 @@
+//Using markers we can set markers on the map and show the info window of the marker on clicking it.
+//Each marker id should be unique.
+//See example below
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Marker> _markers = <Marker>[
+    const Marker(
+      markerId: MarkerId('marker_1'),
+      position: LatLng(
+        30.1799,
+        66.9751,
+      ),
+      infoWindow: InfoWindow(
+        title: "Hello",
+        snippet: "HEHE",
+      ),
+    ),
+    const Marker(
+      markerId: MarkerId('marker_2'),
+      position: LatLng(
+        30.1699,
+        66.9759,
+      ),
+      infoWindow: InfoWindow(
+        title: "Hello2",
+        snippet: "HEHE",
+      ),
+    ),
+    const Marker(
+      markerId: MarkerId('marker_3'),
+      position: LatLng(
+        30.1802,
+        66.9755,
+      ),
+      infoWindow: InfoWindow(
+        title: "Hello3",
+        snippet: "HEHE",
+      ),
+    ),
+  ];
+
+  static const CameraPosition _initialPosition = CameraPosition(
+    target: LatLng(
+      30.1798,
+      66.9750,
+    ),
+    zoom: 15,
+  );
+
+  final Completer<GoogleMapController> _controller = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        markers: Set.of(_markers),
+        initialCameraPosition: _initialPosition,
+        onMapCreated: (controller) {
+          _controller.complete(controller);
+        },
+      ),
+    );
+  }
+}
